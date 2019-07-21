e6c2199928699a4ef5ee68a54ccd401d8a14457f
st3inum <st3inum@gmail.com>
2019-07-21 15:36:08 +0600

    Add files via upload

--- Lab3/Task7.cpp
+++ Lab3/Task7.cpp
@@ -1,7 +1,57 @@
-// Question : Take  a string Find out the unique word count
+// Question : given an string, tokenize them, make hash, and search a word in it and its count
 
 #include<bits/stdc++.h>
 using namespace std;
+struct node{
+    pair<long long,long long> pr;
+    string word;
+    int cnt=1;
+    node(pair<long long,long long> pp,string w)
+    {
+        pr=pp;
+        word=w;
+        cnt=1;
+    }
+    bool operator==(const node& a) const
+    {
+        return (pr == a.pr);
+    }
+    bool operator<(const node& a) const
+    {
+        return (pr < a.pr);
+    }
+};
+pair<long long,long long> hash(string s)
+{
+    long long mod1=1000000+3,mod2=691871;
+    long long sum1=0,sum2=0;
+    for(auto x:s)
+    {
+        sum1+=(sum1*base+x)%mod1;
+        sum1%=mod1;
+
+        sum2+=(sum2*base+x)%mod2;
+        sum2%=mod2;
+    }
+    return {sum1,sum2};
+}
+
+vector<node> v;
+
+void make_count()
+{
+    vector<node> vv;
+    v.push_back(node({-1,-1},"test"));
+    for(int i=0;i+1<v.size();i++)
+    {
+        if(v[i]==v[i+1])v[i+1].cnt=v[i].cnt+1;
+        else
+        {
+            vv.push_back(v[i]);
+        }
+    }
+    v=vv;
+}
 
 int main()
 {
@@ -12,26 +62,15 @@ int main()
     for(auto &x:s)if(x==',' || x=='.' || x=='-')x=' ';
 
     stringstream ss(s);
-    long long mod1=1000000+3,mod2=691871;
-    vector<pair<long long,long long>> v;
+
     while(ss>>a)
     {
-        long long sum1=0,sum2=0;
-        for(auto x:a)
-        {
-            sum1+=(sum1*base+x)%mod1;
-            sum1%=mod1;
-
-            sum2+=(sum2*base+x)%mod2;
-            sum2%=mod2;
-        }
-        v.push_back({sum1,sum2});
+        pair<long long,long long> p=hash(a);
+        node temp(p,a);
+        v.push_back(temp);
     }
     sort(v.begin(),v.end());
-    int cnt=1;
-    for(int i=1;i<v.size();i++)
-    {
-        if(v[i]!=v[i-1])cnt++;
-    }
-    cout<<cnt<<endl;
+    make_count();
+    cin>>s;
+
 }
